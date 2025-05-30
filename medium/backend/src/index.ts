9fb4ed3899c087eb0de466f410f80dfe121a3cc8
rajeshparmar69576 <rp69576@gmail.com>
2025-05-30 17:24:26 +0530

    created api's for user signin and signup using hono

--- medium/backend/src/index.ts
+++ medium/backend/src/index.ts
@@ -1,13 +1,73 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import {sign,verify} from 'hono/jwt'
 
-const app = new Hono()
+const app = new Hono<{
+    Bindings: {
+        DATABASE_URL: string,
+        JWT_SECRET:string,
+    }
+}>()
 
-app.post('/api/v1/user/signup', (c) => {
-  return c.text('thsi is user signup route')
+app.post('/api/v1/user/signup', async (c) => {
+    try {
+        const prisma = new PrismaClient({
+            datasourceUrl: c.env.DATABASE_URL,
+        }).$extends(withAccelerate());
+    
+        const body = await c.req.json();
+    
+        const user = await prisma.user.create({
+            data: {
+                email: body.email,
+                password:body.password
+            },
+        })
+    
+        const token = await sign({ id: user.id }, c.env.JWT_SECRET)
+        return c.json({
+            jwt:token
+        })
+    } catch (e) {
+        c.status(403)
+        return c.json({
+            error:"error while signup"
+        })
+    }
 })
 
-app.post('/api/v1/user/signin', (c) => {
-    return c.text('this is user signin route')
+app.post('/api/v1/user/signin', async (c) => {
+    try {
+        const prisma = new PrismaClient({
+            datasourceUrl: c.env.DATABASE_URL,
+        }).$extends(withAccelerate());
+
+        const body = await c.req.json()
+
+        const user = await prisma.user.findUnique({
+            where: {
+                email: body.email,
+                password:body.password
+            }
+        });
+
+        if (!user) {
+            c.status(403);
+            return c.json({
+                error:"user not found"
+            })
+        }
+
+        const token = await sign({ id: user.id }, c.env.JWT_SECRET)
+        return c.json({
+            jwt: token
+        });
+
+    } catch (e) {
+        c.status(403);
+		return c.json({ error: "error while signing in" });
+    }
 })
 
 
