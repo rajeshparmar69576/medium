4d12751aa89b2417b50784265dddf3c472456490
rajeshparmar69576 <rp69576@gmail.com>
2025-06-05 18:22:09 +0530

    implement Publish Post page

--- medium/frontend/src/App.tsx
+++ medium/frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Signin from "./pages/Signin"
 import  Signup  from "./pages/Signup"
 import Blogs from "./pages/Blogs"
 import Blog from "./pages/Blog"
+import Publish from "./components/Publish"
 
 
 function App() {
@@ -12,10 +13,12 @@ function App() {
     <>
       <Router>
         <Routes>
+          <Route path="/" element={<Signup/>}></Route>  
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog/>} />
           <Route path="/blogs" element={<Blogs/>} />
+          <Route path="/publish" element={<Publish/>} />
         </Routes>
       </Router>
     </>
--- medium/frontend/src/components/Appbar.tsx
+++ medium/frontend/src/components/Appbar.tsx
@@ -4,10 +4,16 @@ import { Link } from "react-router-dom";
 const Appbar = () => {
   return (
     <div className="flex justify-between px-10 py-4 border-b">
-      <Link to={"/blogs"} className="flex flex-col justify-center cursor-pointer">
+      <Link
+        to={"/blogs"}
+        className="flex flex-col justify-center cursor-pointer"
+      >
         Medium
       </Link>
       <div>
+        <Link to={'/publish'}>
+          <button type="button" className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2" >New</button>
+        </Link>
         <Avatar size={"big"} name="harkirat singh" />
       </div>
     </div>
